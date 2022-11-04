afb9ecc4ddee3d0a4886c2aa72db3833b9b83986
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-04 08:46:37 +0300

    Completed Active records-associations Lab

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    characters.map do |character|
+        character.actor.full_name
+    end
+    end
 end
